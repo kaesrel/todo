5cd09fa2332eaf7fb0a122775a0fe52513ccbad5
kaesrel <mr.prom@gmail.com>
2020-10-20 16:28:12 +0700

    Write and revise more tests

--- todo/tests.py
+++ todo/tests.py
@@ -3,33 +3,45 @@ from django.urls import reverse
 from todo.models import Todo
 
 # Create your tests here.
-def create_todo(description, done):
+def create_todo(description, done=False):
     return Todo.objects.create(description=description, done=done)
+
+def get_todo(id):
+    return Todo.objects.get(pk=id)
 
 
 class TodoTests(TestCase):
 
-    def test_1(self):
+    def test_todo_appear_after_creation(self):
         todo = create_todo("Go to class", False)
-        todo.save()
         response = self.client.get(reverse("todo:index"))
         self.assertContains(response, "Go to class")
 
-    def test_2(self):
+    def test_todo_disappear_after_done(self):
         todo = create_todo("Find a computer",False)
-        todo.save()
-        print(todo.done)
-        print(todo.id)
-        # how to invoke a url???
-        response = self.client.get(reverse("todo:done", pk = todo.id))
+        # print(todo.done)
+        # print(todo.id)
+        response = self.client.get(reverse("todo:done", args=(todo.id,)))
+        todo = get_todo(todo.id)
+        response_done = self.client.post(reverse("todo:index"))
+        self.assertNotContains(response_done, "Find a computer")
+        self.assertTrue(todo.done)
 
-        print(todo.done)
-        # self.assertContains(response, "Find a computer")
-        # t = Todo.objects.get(id=todo_id)
-        # response_done = self.client.post('/todo', {'id': })
-        # self.assertNotContains(response_done,"Find a computer")
-        # self.assertContains(response_done,"marked as done")
+    def test_done_redirects_to_index(self):
+        todo = create_todo("Take a walk",False)
+        response = self.client.get(reverse("todo:done", args=(todo.id,)))
+        self.assertEqual(response.url, "/todo/")
 
-    # def test_3(self):
-    #     se
-
+    def test_404_response_with_incorrect_id(self):
+        id = 1
+        # todo = create_todo("Jogging", False)
+        while True:
+            try:
+                todo = get_todo(id)
+            except Todo.DoesNotExist:
+                break
+            id += 1
+        # print("id =", id)
+        response = self.client.get(reverse("todo:done", args=(id,)))
+        self.assertEqual(response.status_code, 404)
+        # print(response.status_code)
